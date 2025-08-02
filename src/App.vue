9146634c839bd9010d053fcbf0f7b66846db1ade
maksVatrenko <maks1004200097@gmail.com>
2025-08-02 14:32:17 +0300

    nuxt init

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Default />
-</template>
-
-<script setup>
-import Default from '@/layouts/default.vue'
-</script>
-
-<style lang="scss"></style>
--- src/views/Index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="page">
-    <div class="page__wrapper">INDEXPAGE</div>
-  </div>
-</template>
-
-<script setup></script>
-
-<style scoped lang="scss"></style>
